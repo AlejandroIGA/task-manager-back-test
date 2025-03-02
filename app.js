8c402c4e3591ecf24bfcb1be6b0b054ef296fdf1
Alejandro Infante <alejandroinfante.ga@gmail.com>
2025-03-02 14:32:13 -0600

    configuracio de ambiente

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, Filter } = require('firebase-admin/firestore');
-const serviceAccount = require('./firebaseKey1.json');
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 const app = express();
 const port = 3000;
